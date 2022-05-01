1cbfa06d494621789901b61606b2b2b17c26830f
kxxt <rsworktech@outlook.com>
2022-05-01 22:31:51 +0800

    feat: better command line interface

--- src/aspeak/__main__.py
+++ src/aspeak/__main__.py
@@ -6,10 +6,22 @@ from . import Synthesizer
 
 parser = argparse.ArgumentParser(description='A simple text-to-speech client using azure TTS API(trial).',
                                  prog='aspeak')
-parser.add_argument('-v', '--version', action='version', version='%(prog)s 0.1.1')
-parser.add_argument('-t', '--text', help='Text to speak.', dest='text', default=None)
-parser.add_argument('-s', '--ssml', help='SSML to speak.', dest='ssml', default=None)
+parser.add_argument('-v', '--version', action='version', version='%(prog)s 0.2.0')
+group = parser.add_mutually_exclusive_group()
+group.add_argument('-t', '--text', help='Text to speak. Left blank when reading from file/stdin.',
+                   dest='text', nargs='?', default=argparse.SUPPRESS)
+group.add_argument('-s', '--ssml', help='SSML to speak. Left blank when reading from file/stdin.',
+                   dest='ssml', nargs='?', default=argparse.SUPPRESS)
+parser.add_argument('-f', '--file', help='Text/SSML file to speak, default to `-`(stdin).', dest='file',
+                    default=argparse.SUPPRESS)
 parser.add_argument('-o', '--output', help='Output wav file path', dest='output_path', default=None)
+
+
+def read_file(args):
+    if not hasattr(args, 'file') or args.file == '-':
+        return sys.stdin.read()
+    with open(args.file, 'r') as f:
+        return f.read()
 
 
 def main():
@@ -19,14 +31,27 @@ def main():
     else:
         audio_config = speechsdk.audio.AudioOutputConfig(filename=args.output_path)
     synthesizer = Synthesizer(audio_config)
-    if args.text is not None and args.ssml is not None:
-        parser.error('`--text` and `--ssml` are mutually exclusive.')
-    if args.ssml is not None:
-        synthesizer.ssml_to_speech(args.ssml)
-    elif args.text is not None:
-        synthesizer.text_to_speech(args.text)
+    if hasattr(args, 'ssml'):
+        if args.ssml is None:
+            # --ssml is provided but empty
+            synthesizer.ssml_to_speech(read_file(args))
+        else:
+            # --ssml is provided and not empty
+            if hasattr(args, 'file'):
+                parser.error('You can only specify one input source.')
+            synthesizer.ssml_to_speech(args.text)
+    elif hasattr(args, 'text'):
+        if args.text is None:
+            # --text is provided but empty
+            synthesizer.text_to_speech(read_file(args))
+        else:
+            # --text is provided and not empty
+            if hasattr(args, 'file'):
+                parser.error('You can only specify one input source.')
+            synthesizer.text_to_speech(args.text)
     else:
-        synthesizer.text_to_speech(sys.stdin.read())
+        # Neither --text nor --ssml is provided, pretend --text is provided and empty
+        synthesizer.text_to_speech(read_file(args))
 
 
 if __name__ == '__main__':
