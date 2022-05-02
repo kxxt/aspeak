e095e4ed194b25747a216c6f7a722bd4ed2087a6
kxxt <rsworktech@outlook.com>
2022-05-02 09:28:34 +0800

    feat: support pitch and rate

--- src/aspeak/__main__.py
+++ src/aspeak/__main__.py
@@ -3,6 +3,7 @@ import azure.cognitiveservices.speech as speechsdk
 import sys
 
 from . import Synthesizer
+from .ssml import create_ssml
 
 parser = argparse.ArgumentParser(
     description='This program uses trial auth token of Azure Cognitive Services to do speech synthesis for you.',
@@ -14,6 +15,11 @@ subgroup.add_argument('-t', '--text', help='Text to speak. Left blank when readi
                       dest='text', nargs='?', default=argparse.SUPPRESS)
 subgroup.add_argument('-s', '--ssml', help='SSML to speak. Left blank when reading from file/stdin.',
                       dest='ssml', nargs='?', default=argparse.SUPPRESS)
+text_group = parser.add_argument_group('Text options')
+text_group.add_argument('-p', '--pitch', help='Set pitch, default to 0', dest='pitch',
+                        type=float, default=argparse.SUPPRESS)
+text_group.add_argument('-r', '--rate', help='Set speech rate, default to 0.04', dest='rate',
+                        type=float, default=argparse.SUPPRESS)
 parser.add_argument('-f', '--file', help='Text/SSML file to speak, default to `-`(stdin).', dest='file',
                     default=argparse.SUPPRESS)
 parser.add_argument('-o', '--output', help='Output wav file path', dest='output_path', default=None)
@@ -28,6 +34,32 @@ def read_file(args):
         return f.read()
 
 
+def preprocess_text(text, args):
+    """
+    Preprocess text.
+    :param text: plain text
+    :param args: args
+    :return: (is_ssml, text_or_ssml)
+    """
+    if hasattr(args, 'pitch') or hasattr(args, 'rate'):
+        if args.voice is None:
+            parser.error('Voice must be specified when using pitch or rate.')
+        pitch = args.pitch if hasattr(args, 'pitch') else 0.0
+        rate = args.rate if hasattr(args, 'rate') else 0.04
+        voice = args.voice if hasattr(args, 'voice') else None
+        ssml = create_ssml(text, voice, rate, pitch)
+        return True, ssml
+    return False, text
+
+
+def speech_function_selector(synthesizer, preprocessed):
+    is_ssml, text_or_ssml = preprocessed
+    if is_ssml:
+        return synthesizer.ssml_to_speech(text_or_ssml)
+    else:
+        return synthesizer.text_to_speech(text_or_ssml)
+
+
 def main():
     args = parser.parse_args()
     if args.output_path is None:
@@ -36,6 +68,8 @@ def main():
         audio_config = speechsdk.audio.AudioOutputConfig(filename=args.output_path)
     synthesizer = Synthesizer(audio_config, args.locale, args.voice)
     if hasattr(args, 'ssml'):
+        if hasattr(args, 'rate') or hasattr(args, 'pitch'):
+            parser.error('You can only use --rate and --pitch with --text. Please set these settings in your SSML.')
         if args.ssml is None:
             # --ssml is provided but empty
             synthesizer.ssml_to_speech(read_file(args))
@@ -47,12 +81,12 @@ def main():
     elif hasattr(args, 'text'):
         if args.text is None:
             # --text is provided but empty
-            synthesizer.text_to_speech(read_file(args))
+            speech_function_selector(synthesizer, preprocess_text(read_file(args), args))
         else:
             # --text is provided and not empty
             if hasattr(args, 'file'):
                 parser.error('You can only specify one input source.')
-            synthesizer.text_to_speech(args.text)
+            speech_function_selector(synthesizer, preprocess_text(args.text, args))
     else:
         # Neither --text nor --ssml is provided, pretend --text is provided and empty
         synthesizer.text_to_speech(read_file(args))
---
+++ src/aspeak/ssml.py
@@ -0,0 +1,19 @@
+from xml.sax.saxutils import escape
+
+
+def create_ssml(text, voice, rate, pitch):
+    """
+    Create SSML for text to be spoken.
+    """
+    ssml = '''<speak 
+    xmlns="http://www.w3.org/2001/10/synthesis"
+    xmlns:mstts="http://www.w3.org/2001/mstts"
+    xmlns:emo="http://www.w3.org/2009/10/emotionml"
+    version="1.0" xml:lang="en-US">
+    '''
+    ssml += f'<voice name="{voice}">' if voice is not None else '<voice>'
+    ssml += f'<mstts:express-as style="general">'
+    ssml += f'<prosody rate="{round(rate * 100)}%" pitch="{round(pitch * 100)}%">'
+    ssml += escape(text)
+    ssml += '</prosody></mstts:express-as></voice></speak>'
+    return ssml
