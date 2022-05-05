c3c2663b2916fbfbd3cfd73df32ad479019953ab
kxxt <rsworktech@outlook.com>
2022-05-05 11:59:09 +0800

    chore: bump version

--- src/aspeak/__main__.py
+++ src/aspeak/__main__.py
@@ -10,7 +10,7 @@ parser = argparse.ArgumentParser(
     description='This program uses trial auth token of Azure Cognitive Services to do speech synthesis for you',
     prog='aspeak')
 group = parser.add_mutually_exclusive_group()
-group.add_argument('-V', '--version', action='version', version='%(prog)s 1.1.1')
+group.add_argument('-V', '--version', action='version', version='%(prog)s 1.1.2')
 group.add_argument('-L', '--list-voices', action='store_true',
                    help='list available voices, you can combine this argument with -v and -l', dest='list_voices')
 subgroup = group.add_mutually_exclusive_group()
