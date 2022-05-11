d14e2ca6d8b75b2266e839d331fed3e01bd5cd1b
kxxt <rsworktech@outlook.com>
2022-05-11 16:45:36 +0800

    refactor: idiomatic python

--- src/aspeak/__main__.py
+++ src/aspeak/__main__.py
@@ -111,7 +111,7 @@ def validate_quality(args, parser):
     if hasattr(args, 'format') and args.quality != 0:
         parser.error("You can't use --quality with --format.")
     for ext in {"mp3", "ogg", "wav", "webm"}:
-        if getattr(args, ext) and not args.quality in QUALITIES[ext]:
+        if getattr(args, ext) and args.quality not in QUALITIES[ext]:
             parser.error(f"Invalid quality {args.quality} for {ext}.")
 
 
