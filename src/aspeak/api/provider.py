dc6243d81b3f90d96266b6e4e0ea5dcd17bb7c10
kxxt <rsworktech@outlook.com>
2022-05-13 12:42:53 +0800

    feat: impl SpeechServiceProvider

--- src/aspeak/api/provider.py
+++ src/aspeak/api/provider.py
@@ -1,2 +1,33 @@
+import azure.cognitiveservices.speech as speechsdk
+
+from ..token import Token
+
+
 class SpeechServiceProvider:
-    pass
+    def __init__(self):
+        self._current_token = Token()
+
+    @property
+    def token(self) -> Token:
+        if self._expired:
+            self.renew()
+        return self._current_token
+
+    @property
+    def _expired(self) -> bool:
+        return self._current_token.expired()
+
+    def renew(self) -> None:
+        self._current_token.renew()
+
+    def text_to_speech(self, text: str, cfg: speechsdk.SpeechConfig,
+                       output: speechsdk.audio.AudioOutputConfig) -> speechsdk.SpeechSynthesisResult:
+        if self._expired:
+            self.renew()
+        return speechsdk.SpeechSynthesizer(speech_config=cfg, audio_config=output).speak_text(text)
+
+    def ssml_to_speech(self, ssml: str, cfg: speechsdk.SpeechConfig,
+                       output: speechsdk.audio.AudioOutputConfig) -> speechsdk.SpeechSynthesisResult:
+        if self._expired:
+            self.renew()
+        return speechsdk.SpeechSynthesizer(speech_config=cfg, audio_config=output).speak_ssml(ssml)
