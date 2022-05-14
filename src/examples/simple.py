2d65daa65d456c27aee49109bfd8e81241ae63b8
kxxt <rsworktech@outlook.com>
2022-05-14 18:52:42 +0800

    fix: example simple.py

--- src/examples/simple.py
+++ src/examples/simple.py
@@ -7,4 +7,4 @@ output = AudioOutputConfig(use_default_speaker=True)
 if __name__ == "__main__":
     text_to_speech(provider, output, "Hello World! I am using aspeak to synthesize speech.",
                    voice="en-US-JennyNeural", rate="+10%", pitch="-5%", style="cheerful",
-                   audio_format=AudioFormat.from_enum_and_quality(FileFormat.WAV, 1))
+                   audio_format=AudioFormat(FileFormat.WAV, 1))
