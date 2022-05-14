1a4bf11eb3ac9658d81d4ff2bba501b243c46005
kxxt <rsworktech@outlook.com>
2022-05-14 14:48:41 +0800

    feat: example for functional API

---
+++ src/examples/simple.py
@@ -0,0 +1,10 @@
+from aspeak import SpeechServiceProvider, text_to_speech, AudioFormat, FileFormat
+from azure.cognitiveservices.speech.audio import AudioOutputConfig
+
+provider = SpeechServiceProvider()
+output = AudioOutputConfig(use_default_speaker=True)
+
+if __name__ == "__main__":
+    text_to_speech(provider, output, "Hello World! I am using aspeak to synthesize speech.",
+                   voice="en-US-JennyNeural", rate="+10%", pitch="-5%", style="cheerful",
+                   audio_format=AudioFormat.from_enum_and_quality(FileFormat.WAV, 1))
