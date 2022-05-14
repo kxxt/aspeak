2c8c7a91a1872e0831cf267133956bdd5e86c76a
kxxt <rsworktech@outlook.com>
2022-05-14 16:20:09 +0800

    rename: ASpeakError -> AspeakError

--- src/aspeak/errors.py
+++ src/aspeak/errors.py
@@ -1,8 +1,8 @@
-class ASpeakError(Exception):
+class AspeakError(Exception):
     pass
 
 
-class TokenRetrievalError(ASpeakError):
+class TokenRetrievalError(AspeakError):
     def __init__(self, status_code, message="Failed to retrieve token"):
         super(TokenRetrievalError, self).__init__(message)
         self.status_code = status_code
