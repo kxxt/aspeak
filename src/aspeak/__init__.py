94175346fbbbe94de35fe8ce00d6b14360372c1a
kxxt <rsworktech@outlook.com>
2022-05-15 22:09:37 +0800

    docs: module docstring

--- src/aspeak/__init__.py
+++ src/aspeak/__init__.py
@@ -1,3 +1,8 @@
+"""
+aspeak is a CLI tool as well as a Python library that
+uses trial auth token of Azure Cognitive Services to do speech synthesis for you.
+"""
+
 from .auth import get_token_info_dict
 from .token import Token
 from .api import *
