b4aaafb6edb5e328e29bcf71bb47fb46f604a367
kxxt <rsworktech@outlook.com>
2022-05-16 09:07:24 +0800

    docs: in code docs for errors.py

--- src/aspeak/errors.py
+++ src/aspeak/errors.py
@@ -1,8 +1,14 @@
 class AspeakError(Exception):
-    pass
+    """
+    Base class for all Aspeak errors.
+    """
 
 
 class TokenRetrievalError(AspeakError):
+    """
+    Error raised when the trial token cannot be retrieved.
+    """
+
     def __init__(self, status_code, message="Failed to retrieve token"):
         super(TokenRetrievalError, self).__init__(message)
         self.status_code = status_code
