7d01ec332f3a37e5f8c0100d9d68920b9ce740c7
kxxt <rsworktech@outlook.com>
2022-05-16 09:07:55 +0800

    fix: python 3 flavor super()

--- src/aspeak/errors.py
+++ src/aspeak/errors.py
@@ -10,6 +10,6 @@ class TokenRetrievalError(AspeakError):
     """
 
     def __init__(self, status_code, message="Failed to retrieve token"):
-        super(TokenRetrievalError, self).__init__(message)
+        super().__init__(message)
         self.status_code = status_code
         self.message = message
