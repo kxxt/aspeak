6071035ae89df5255a41a7336176c21a628f66e4
kxxt <rsworktech@outlook.com>
2022-05-16 09:04:34 +0800

    fix: use a seq type for iterating

--- src/aspeak/cli/validation.py
+++ src/aspeak/cli/validation.py
@@ -6,7 +6,7 @@ def validate_quality(args, parser):
         return
     if hasattr(args, 'format') and args.quality != 0:
         parser.error("You can't use --quality with --format.")
-    for ext in {"mp3", "ogg", "wav", "webm"}:
+    for ext in ["mp3", "ogg", "wav", "webm"]:
         if getattr(args, ext) and args.quality not in QUALITIES[ext]:
             parser.error(f"Invalid quality {args.quality} for {ext}.")
 
