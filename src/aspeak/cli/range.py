c101b4518bea19c24d12937b68477bd23e901bea
kxxt <rsworktech@outlook.com>
2022-05-16 09:20:46 +0800

    fix: pylint line-too-long

--- src/aspeak/cli/range.py
+++ src/aspeak/cli/range.py
@@ -1,4 +1,5 @@
 class Range(object):
+    # pylint: disable=line-too-long
     """
     A range of values for argparse.
     reference: https://stackoverflow.com/questions/12116685/how-can-i-require-my-python-scripts-argument-to-be-a-float-in-a-range-using-arg
