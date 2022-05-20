6773d572ceca755e0abab87ac51aa7985709df4d
kxxt <rsworktech@outlook.com>
2022-05-20 10:33:46 +0800

    fix(example): move provider creation into try block

--- src/examples/line_by_line.py
+++ src/examples/line_by_line.py
@@ -6,13 +6,12 @@ from azure.cognitiveservices.speech import ResultReason
 
 from aspeak import SpeechServiceProvider, pure_text_to_speech, AspeakError
 
-provider = SpeechServiceProvider()
 output = AudioOutputConfig(use_default_speaker=True)
-
-tts = partial(pure_text_to_speech, provider, output)
 
 if __name__ == "__main__":
     try:
+        provider = SpeechServiceProvider()
+        tts = partial(pure_text_to_speech, provider, output)
         while True:
             result = tts(input("Enter text to speak: "))
             if result.reason != ResultReason.SynthesizingAudioCompleted:
--- src/examples/simple.py
+++ src/examples/simple.py
@@ -3,11 +3,11 @@ from azure.cognitiveservices.speech import ResultReason
 
 from aspeak import SpeechServiceProvider, text_to_speech, AudioFormat, FileFormat, AspeakError
 
-provider = SpeechServiceProvider()
 output = AudioOutputConfig(use_default_speaker=True)
 
 if __name__ == "__main__":
     try:
+        provider = SpeechServiceProvider()
         result = text_to_speech(provider, output, "Hello World! I am using aspeak to synthesize speech.",
                                 voice="en-US-JennyNeural", rate="+10%", pitch="-5%", style="cheerful",
                                 audio_format=AudioFormat(FileFormat.WAV, 1))
