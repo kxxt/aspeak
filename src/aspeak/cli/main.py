968f709d566131bbdcc5af41d5b4bdd94b6c31d2
kxxt <rsworktech@outlook.com>
2022-09-02 02:52:53 +0800

    feat: migrate cli to new api

--- src/aspeak/cli/main.py
+++ src/aspeak/cli/main.py
@@ -3,14 +3,12 @@ from functools import partial
 
 import azure.cognitiveservices.speech as speechsdk
 
-from ..api import SpeechServiceProvider, text_to_speech, pure_text_to_speech, ssml_to_speech
+from ..api.api import SpeechToSpeakerService, SpeechToOneFileService
 from ..ssml import create_ssml
-from ..errors import AspeakError
 from .voices import list_voices
 from .utils import list_qualities_and_formats
 from ..quality import QUALITIES
 from .parser import parser
-from .constants import COLOR_RED, COLOR_CLEAR
 from .validation import validate_quality, get_ineffective_args_for_listing, has_text_options
 from .result_handler import handle_result
 
@@ -27,7 +25,7 @@ def preprocess_text(text, args):
     Preprocess text.
     :param text: plain text
     :param args: args
-    :return: (is_ssml, text_or_ssml, extra_options)
+    :return: (is_ssml, text_or_ssml)
     """
     if has_text_options(args):
         if args.voice is None:
@@ -39,42 +37,41 @@ def preprocess_text(text, args):
         role = args.role if hasattr(args, 'role') else None
         style_degree = args.style_degree if hasattr(args, 'style_degree') else None
         ssml = create_ssml(text, voice, rate, pitch, style, style_degree, role)
-        return True, ssml, None
-    return False, text, {'locale': args.locale, 'voice': args.voice}
+        return True, ssml
+    return False, text
 
 
-def speech_function_selector(funcs, preprocessed, audio_format):
-    _ssml_to_speech, _pure_text_to_speech = funcs
-    is_ssml, text_or_ssml, options = preprocessed
+def speech_function_selector(speech_service, preprocessed):
+    is_ssml, text_or_ssml = preprocessed
     if is_ssml:
-        return _ssml_to_speech(text_or_ssml, audio_format)
-    return _pure_text_to_speech(text_or_ssml, audio_format=audio_format, **options)
+        return speech_service.ssml_to_speech(text_or_ssml)
+    return speech_service.pure_text_to_speech(text_or_ssml)
 
 
-def main_ssml(func, args, audio_format):
+def main_ssml(speech, args):
     if hasattr(args, 'rate') or hasattr(args, 'pitch') or hasattr(args, 'style'):
         parser.error(
             'You can only use text options with --text. Please set these settings in your SSML.')
     if args.ssml is None:
         # --ssml is provided but empty
-        result = func(read_file(args), audio_format)
+        result = speech.ssml_to_speech(read_file(args))
     else:
         # --ssml is provided and not empty
         if hasattr(args, 'file'):
             parser.error('You can only specify one input source.')
-        result = func(args.text, audio_format)
+        result = speech.ssml_to_speech(args.text)
     return result
 
 
-def main_text(funcs, args, audio_format):
+def main_text(speech, args):
     if args.text is None:
         # --text is provided but empty
-        result = speech_function_selector(funcs, preprocess_text(read_file(args), args), audio_format)
+        result = speech_function_selector(speech, preprocess_text(read_file(args), args))
     else:
         # --text is provided and not empty
         if hasattr(args, 'file'):
             parser.error('You can only specify one input source.')
-        result = speech_function_selector(funcs, preprocess_text(args.text, args), audio_format)
+        result = speech_function_selector(speech, preprocess_text(args.text, args))
     return result
 
 
@@ -97,11 +94,6 @@ def main_list_voices(args):
 
 
 def setup_defaults(args):
-    if args.output_path is None:
-        args.audio_config = speechsdk.audio.AudioOutputConfig(use_default_speaker=True)
-    else:
-        args.audio_config = speechsdk.audio.AudioOutputConfig(filename=args.output_path)
-
     args.locale = args.locale if hasattr(args, 'locale') else None
     args.voice = args.voice if hasattr(args, 'voice') else None
     args.quality = args.quality if hasattr(args, 'quality') else 0
@@ -138,20 +130,18 @@ def main():
     else:
         audio_format = QUALITIES[args.file_type][args.quality]
 
-    try:
-        provider = SpeechServiceProvider()
-        _text_to_speech = partial(text_to_speech, provider, args.audio_config)
-        _ssml_to_speech = partial(ssml_to_speech, provider, args.audio_config)
-        _pure_text_to_speech = partial(pure_text_to_speech, provider, args.audio_config)
-        funcs = _ssml_to_speech, _pure_text_to_speech
-        if hasattr(args, 'ssml'):
-            result = main_ssml(_ssml_to_speech, args, audio_format)
-        elif hasattr(args, 'text'):
-            result = main_text(funcs, args, audio_format)
-        else:
-            # Neither --text nor --ssml is provided, pretend --text is provided and empty
-            result = speech_function_selector(funcs, preprocess_text(read_file(args), args), audio_format)
-        handle_result(result)
-    except AspeakError as exception:
-        print(f"{COLOR_RED}Error{COLOR_CLEAR}: {exception}")
-        sys.exit(4)
+    if args.output_path is None:
+        create_speech = SpeechToSpeakerService
+    else:
+        create_speech = partial(SpeechToOneFileService, args.output_path)
+
+    speech = create_speech(args.locale, args.voice, audio_format)
+
+    if hasattr(args, 'ssml'):
+        result = main_ssml(speech, args)
+    elif hasattr(args, 'text'):
+        result = main_text(speech, args)
+    else:
+        # Neither --text nor --ssml is provided, pretend --text is provided and empty
+        result = speech_function_selector(speech, preprocess_text(read_file(args), args))
+    handle_result(result)
