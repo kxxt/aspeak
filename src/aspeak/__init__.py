e064256a8a22a17352fb20e357d14a39226ff312
kxxt <rsworktech@outlook.com>
2022-09-06 07:49:18 +0800

    lint: make pylint happy

--- src/aspeak/__init__.py
+++ src/aspeak/__init__.py
@@ -3,6 +3,7 @@ aspeak is a CLI tool as well as a Python library that
 uses trial auth token of Azure Cognitive Services to do speech synthesis for you.
 """
 
-from .api import *
 # Re-export some common types to simplify imports on the user side
-from azure.cognitiveservices.speech import SpeechSynthesisOutputFormat, ResultReason, CancellationReason, ResultFuture
+from azure.cognitiveservices.speech import SpeechSynthesisOutputFormat, \
+    ResultReason, CancellationReason, ResultFuture
+from .api import *
