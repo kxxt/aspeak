7045a8320e8941ab1030fe355c732e2889945994
flt6 <1404262047@qq.com>
2023-02-03 09:54:57 +0800

    🐞 fix(urls): Fix token can't get
    https://github.com/kxxt/aspeak/issues/42

--- src/aspeak/urls.py
+++ src/aspeak/urls.py
@@ -1,4 +1,4 @@
-GET_TOKEN="https://azure.microsoft.com/zh-cn/products/cognitive-services/speech-to-text/"
+GET_TOKEN="https://azure.microsoft.com/zh-cn/products/cognitive-services/speech-translation/"
 
 def voice_list_url() -> str:
     return f'https://eastus.api.speech.microsoft.com/cognitiveservices/voices/list'
