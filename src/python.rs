2f22b0d19374aa61456364672d7df75fb2046d12
kxxt <rsworktech@outlook.com>
2023-02-06 10:15:15 +0800

    pause python binding's development

--- src/python.rs
+++ src/python.rs
@@ -2,8 +2,7 @@ use pyo3::exceptions::PyValueError;
 use pyo3::{prelude::*, types::PyDict};
 
 use crate::{
-    callback_play_blocking, parse_pitch, parse_rate, validate_style_degree, AudioFormat,
-    Synthesizer, SynthesizerConfig, TextArgs, DEFAULT_ENDPOINT,
+    callback_play_blocking, AudioFormat, Synthesizer, SynthesizerConfig, DEFAULT_ENDPOINT,
 };
 
 #[pymodule]
@@ -13,30 +12,32 @@ fn aspeak(py: Python, m: &PyModule) -> PyResult<()> {
     Ok(())
 }
 
-// #[pymethods]
-// impl SynthesizerConfig {
-//     #[new]
-//     #[pyo3(signature = (audio_format,**options))]
-//     fn pynew(audio_format: AudioFormat, options: Option<&PyDict>) -> PyResult<Self> {
-//         let endpoint = options
-//             .and_then(|dict| dict.get_item("endpoint"))
-//             .map(|endpoint| endpoint.extract::<String>())
-//             .transpose()?;
-//         Ok(Self {
-//             auth: todo!(),
-//             audio_format,
-//         })
-//     }
+#[pyclass(name = "SynthesizerConfig")]
+struct PySynthesizerConfig();
 
-//     #[pyo3(name = "connect")]
-//     fn pyconnect(&self) -> PyResult<Synthesizer> {
-//         let rt = tokio::runtime::Builder::new_current_thread()
-//             .enable_all()
-//             .build()?;
-//         let config = self.clone();
-//         Ok(rt.block_on(config.connect())?)
-//     }
-// }
+impl PySynthesizerConfig {}
+
+#[pymethods]
+impl PySynthesizerConfig {
+    #[new]
+    #[pyo3(signature = (audio_format,**options))]
+    fn new(audio_format: AudioFormat, options: Option<&PyDict>) -> PyResult<Self> {
+        let endpoint = options
+            .and_then(|dict| dict.get_item("endpoint"))
+            .map(|endpoint| endpoint.extract::<String>())
+            .transpose()?;
+        Ok(Self {})
+    }
+
+    fn connect(&self) -> PyResult<Synthesizer> {
+        let rt = tokio::runtime::Builder::new_current_thread()
+            .enable_all()
+            .build()?;
+        let config = self.clone();
+        todo!()
+        // Ok(rt.block_on(config.connect())?)
+    }
+}
 
 #[pymethods]
 impl Synthesizer {
@@ -52,45 +53,45 @@ impl Synthesizer {
     }
 }
 
-#[pymethods]
-impl TextArgs {
-    #[new]
-    #[pyo3(signature = (**kwargs))]
-    fn new(kwargs: Option<&PyDict>) -> PyResult<Self> {
-        let mut options = Self::default();
-        if let Some(dict) = kwargs {
-            if let Some(text) = dict.get_item("text") {
-                options.text = Some(text.extract()?);
-            }
-            if let Some(pitch) = dict.get_item("pitch") {
-                options.pitch =
-                    Some(parse_pitch(pitch.extract()?).map_err(|e| PyValueError::new_err(e))?);
-            }
-            if let Some(rate) = dict.get_item("rate") {
-                options.rate =
-                    Some(parse_rate(rate.extract()?).map_err(|e| PyValueError::new_err(e))?);
-            }
-            if let Some(style) = dict.get_item("style") {
-                options.style = Some(style.extract()?);
-            }
-            if let Some(role) = dict.get_item("role") {
-                options.role = Some(role.extract()?);
-            }
-            if let Some(style_degree) = dict.get_item("style_degree") {
-                let degree: f32 = style_degree.extract()?;
-                if !validate_style_degree(degree) {
-                    return Err(PyValueError::new_err("Style degree out of range [0.01, 2]"));
-                }
-                options.style_degree = Some(degree);
-            }
-            if let Some(locale) = dict.get_item("locale") {
-                // todo: default voice for locale
-                options.locale = Some(locale.extract()?);
-            }
-            if let Some(voice) = dict.get_item("voice") {
-                options.voice = Some(voice.extract()?);
-            }
-        }
-        Ok(options)
-    }
-}
+// #[pymethods]
+// impl TextArgs {
+//     #[new]
+//     #[pyo3(signature = (**kwargs))]
+//     fn new(kwargs: Option<&PyDict>) -> PyResult<Self> {
+//         let mut options = Self::default();
+//         if let Some(dict) = kwargs {
+//             if let Some(text) = dict.get_item("text") {
+//                 options.text = Some(text.extract()?);
+//             }
+//             if let Some(pitch) = dict.get_item("pitch") {
+//                 options.pitch =
+//                     Some(parse_pitch(pitch.extract()?).map_err(|e| PyValueError::new_err(e))?);
+//             }
+//             if let Some(rate) = dict.get_item("rate") {
+//                 options.rate =
+//                     Some(parse_rate(rate.extract()?).map_err(|e| PyValueError::new_err(e))?);
+//             }
+//             if let Some(style) = dict.get_item("style") {
+//                 options.style = Some(style.extract()?);
+//             }
+//             if let Some(role) = dict.get_item("role") {
+//                 options.role = Some(role.extract()?);
+//             }
+//             if let Some(style_degree) = dict.get_item("style_degree") {
+//                 let degree: f32 = style_degree.extract()?;
+//                 if !validate_style_degree(degree) {
+//                     return Err(PyValueError::new_err("Style degree out of range [0.01, 2]"));
+//                 }
+//                 options.style_degree = Some(degree);
+//             }
+//             if let Some(locale) = dict.get_item("locale") {
+//                 // todo: default voice for locale
+//                 options.locale = Some(locale.extract()?);
+//             }
+//             if let Some(voice) = dict.get_item("voice") {
+//                 options.voice = Some(voice.extract()?);
+//             }
+//         }
+//         Ok(options)
+//     }
+// }
