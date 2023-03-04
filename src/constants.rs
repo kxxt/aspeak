d10b3d4b132473ed7cc6395d5379449333ae3790
kxxt <rsworktech@outlook.com>
2023-03-04 09:01:39 +0800

    lint: allow unused constants

--- src/constants.rs
+++ src/constants.rs
@@ -1,3 +1,4 @@
 pub const ORIGIN: &str = "https://azure.microsoft.com";
+#[allow(unused)]
 pub const DEFAULT_ENDPOINT: &str =
     "wss://eastus.api.speech.microsoft.com/cognitiveservices/websocket/v1";
