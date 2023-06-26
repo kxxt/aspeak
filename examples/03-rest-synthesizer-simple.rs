c761000f1f82731267e204fab4db1af8276b852e
kxxt <rsworktech@outlook.com>
2023-06-26 16:41:03 +0800

    example: 03-rest-synthesizer-simple

---
+++ examples/03-rest-synthesizer-simple.rs
@@ -0,0 +1,33 @@
+use aspeak::{
+    get_rest_endpoint_by_region, synthesizer::SynthesizerConfig, AudioFormat, AuthOptionsBuilder,
+    TextOptionsBuilder,
+};
+
+use std::error::Error;
+
+use tokio::{fs::File, io::AsyncWriteExt};
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let auth = AuthOptionsBuilder::new(
+        get_rest_endpoint_by_region("eastus"), // Use eastus endpoint, RESTful API
+    )
+    .key("YOUR_AZURE_SUBSCRIPTION_KEY")
+    .build();
+    let config = SynthesizerConfig::new(auth, AudioFormat::Riff16Khz16BitMonoPcm);
+    let rest_syn = config.rest_synthesizer()?;
+    let ssml = r#"<speak version="1.0" xmlns="http://www.w3.org/2001/10/synthesis" xml:lang="en-US"><voice name="en-US-JennyNeural">Hello, world!</voice></speak>"#;
+    let audio_data = rest_syn.synthesize_ssml(ssml).await?;
+    let mut file = File::create("examples/ssml-output.wav").await?;
+    file.write_all(&audio_data).await?;
+    let text = "Hello, world!";
+    let options = TextOptionsBuilder::new()
+        .voice("en-US-JennyNeural")
+        .rate("fast")
+        .pitch("high")
+        .build();
+    let audio_data = rest_syn.synthesize_text(text, &options).await?;
+    let mut file = File::create("examples/text-output.wav").await?;
+    file.write_all(&audio_data).await?;
+    Ok(())
+}
